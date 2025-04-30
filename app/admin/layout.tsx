bef1baf146924adc837f4eae6b7dd8632196e50a
Javkhaa <bayarjavkhlanbatsukh.com>
2025-04-30 12:48:33 +0800

    jack's 2nd commit

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,11 +2,11 @@ import AdminSidebar from "@/components/admin/sidebar";
 
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
     return (
-        <div className="flex h-screen w-full">
-            <div className="w-[15%]">
+        <div className="flex h-screen w-[100%]">
+            <div className=" flex w-[15%]">
                 <AdminSidebar />
             </div>
-            <main className="flex-1 p-8 bg-gray-100 w-[85%]">
+            <main className="flex-1 p-8 w-[85%] overflow-y-auto">
                 {children}
             </main>
         </div>
--- components/admin/sidebar.tsx
+++ components/admin/sidebar.tsx
@@ -1,16 +1,33 @@
-import Link from 'next/link';
-
+import Link from "next/link";
+import MenuIcon from "./menu";
 export default function AdminSidebar() {
-    return (
-        <div className="flex flex-col w-64 h-screen bg-gray-900 text-white shadow-lg fixed top-0 left-0">
-            <div className="px-6 py-4">
-                <h1 className="text-2xl font-bold">БичигAI</h1>
-            </div>
-            <nav className="flex-1 px-4 py-6 space-y-2">
-                <Link href="/admin/home" className="block px-4 py-2 rounded hover:bg-gray-700 transition">Нүүр</Link>
-                <Link href="/admin/users" className="block px-4 py-2 rounded hover:bg-gray-700 transition">Хэрэглэгч</Link>
-                <Link href="/admin/premium" className="block px-4 py-2 rounded hover:bg-gray-700 transition">Төлбөрт</Link>
-            </nav>
+  return (
+    <div className="flex flex-col w-64 h-screen bg-[#07173C] text-white shadow-lg fixed top-0 left-0">
+      <div className="px-6 py-4">
+        <h1 className="text-2xl font-bold">БичигAI</h1>
+      </div>
+      <nav className="flex-1 px-4 py-6 space-y-2">
+        <div className="flex items-center  mb-4 gap-4 block px-4 py-2 rounded hover:bg-gray-700 transition">
+          <MenuIcon />
+          <Link href="/admin/home" className="text-[20px]">
+            Нүүр
+          </Link>
         </div>
-    );
+        <div className="flex items-center  mb-4 gap-4 block px-4 py-2 rounded hover:bg-gray-700 transition">
+          <Link
+            href="/admin/users"
+          >
+            Хэрэглэгч
+          </Link>
+        </div>
+        <div className="flex items-center  mb-4 gap-4 block px-4 py-2 rounded hover:bg-gray-700 transition">
+          <Link
+            href="/admin/premium"
+          >
+            Төлбөрт
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
